c42c5ebde43569f51ef195cc14f5c9a29ecff452
한정석 <wjdtjrrkt8888@gmail.com>
2024-08-20 15:09:24 +0900

    나의 공부 일기 개발환경 구축

---
+++ src/main/java/com/example/mystudydiary/MyStudyDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.mystudydiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudyDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStudyDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mystudydiary/MyStudyDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mystudydiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStudyDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
